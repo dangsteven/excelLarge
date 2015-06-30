f08a9fdd47163b671bdab8efa6ea8177dafc652f
silkypanda <sddang@uh.edu>
2015-06-30 09:26:36 -0500

    Update dataRetriever.py

--- dataRetriever.py
+++ dataRetriever.py
@@ -3,7 +3,7 @@ import sqlite3
 import time
 
 def connectOracle():
-	con = cx_Oracle.connect('CER_PMO_RO/cerpmoro@CERINP')
+	con = cx_Oracle.connect('defaultdatabase')
 	cur = con.cursor()
 	cur.execute("SELECT * FROM defaultDB")
 
